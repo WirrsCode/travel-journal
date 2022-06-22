5d9fe343684ce0d7ac878d7c34567fa185e8b529
Will Chen <chilliam.wen@gmail.com>
2022-06-22 16:37:27 -0700

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+export default function Card(props) {
+    let date
+    if (props.endDate) {
+        date = props.startDate + " - " + props.endDate
+    } 
+    else {
+        date = props.startDate
+    }
+
+    return (
+        <div className="card">
+            <span className="card-section">
+                <img src={props.imageUrl} alt={props.imageUrl} className="card--img" />
+                <span className="card--content">
+                    <span className="card--country">
+                        <img src="../images/Fill 219.png" alt="pin icon" className="card--pin"/>
+                        <span className="card--country-name">{props.location.toUpperCase()}</span>
+                        <a href={props.googleMapsUrl} target="_blank" rel="noreferrer">
+                            View on Google Maps
+                        </a>
+                    </span>
+                    <h1>{props.title}</h1>
+                    <h2>{date}</h2>
+                    <p>{props.description}</p>
+                    <div class="tooltip">Read more...
+                        <span class="tooltiptext">Jk, I didn't write anything else</span>
+                    </div>
+                </span>
+            </span>
+            <hr />
+        </div>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export default [
+    {
+        id: 0,
+        title: "Half Moon Bay",
+        location: "California",
+        googleMapsUrl: "https://goo.gl/maps/LLPpLjVk4DjT9VuSA",
+        startDate: "11 May, 2022",
+        description: "We went to half moon bay and did some stuff.",
+        imageUrl: "https://lh3.googleusercontent.com/pw/AM-JKLXFk6UFrQyDdzN3kA35JWgCAldEvl9HNEKewnuJouP_Inlj-qNUC5Pa3mdIJ3he8qtAm4TaGGehcCtLVi8QYn4bV5M5nqeJ-g70Yu1cHemdRsoF20NPrUiP7EqFyR9zqsL5eqJCXn7E42ODuwTmqCrTDA=w785-h944-no?authuser=0"
+    },
+    {
+        id: 1,
+        title: "San Mateo",
+        location: "California",
+        googleMapsUrl: "https://goo.gl/maps/65pafphBK4FP8vNH6",
+        startDate: "15 May, 2022",
+        description: "We went to a park in San Mateo and did stuff.",
+        imageUrl: "https://lh3.googleusercontent.com/pw/AM-JKLWtSoF4L2UlVhSpFdHkSxWi8JhQlpTM2l72pTTsEF5UEBgXkQ8pT3LHZqGgxSgMRz-fikl6OjPA2OmBJHTnvYXt-0ZDgc1OceeLJWMtSNFHnxYrz4eL9QkuhcNF2bXAcEr-lMngy28EpAfoY86dtUlHQA=w1145-h859-no?authuser=0"
+    },
+    {
+        id: 2,
+        title: "Edgewood Park & Natural Preserve",
+        location: "California",
+        googleMapsUrl: "https://goo.gl/maps/N8CTMy9HSKxXSVrJ8",
+        startDate: "16 May, 2022",
+        description: "We went to do a short hike, but now this area caught on fire :C",
+        imageUrl: "https://lh3.googleusercontent.com/pw/AM-JKLWZRpiqlSVyUwyeDvaP0wiENhAoGkFK5J0JzR7-Wiy-mHiksATdyzltT9_JRymaPLszSiF4-vN91Fa7w2sIhowyYzbnv5NRYuqjFZowviZ2x6x0HCC-prg9U_tz6wL5SUeXvxax9ZsuElwOjeSZaVBFlQ=w1136-h1311-no?authuser=0"
+    },
+    {
+        id: 3,
+        title: "San Luis Obispo",
+        location: "California",
+        googleMapsUrl: "https://goo.gl/maps/8ubb6jCrExGLBEuF7",
+        startDate: "4 June, 2022",
+        endDate: "5 June, 2022",
+        description: "I crashed your friend trip and then we did stuff.",
+        imageUrl: "https://lh3.googleusercontent.com/pw/AM-JKLXj4QICIEnbgs2doCK5AlIkzDN2TDJ1AWFZJHXazB6GFE2BJnbrkRM5hv0XkY--sNd4xOflq1VP4sH40_SSf15HXffDJuYpzTVEPLQonRQJoq5zuuNrAeVb8wrdJWoo-pABvIhiuZiYzmPgutyE8OwYqA=w984-h1311-no?authuser=0"
+    },
+]
